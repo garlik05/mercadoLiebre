98487dc998f3824a930ea64585b1544a8e406048
garlik05 <garlikage@protonmail.com>
2022-04-19 13:41:52 -0300

    para empezar

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname,'views','index.html'))
+});
+
+
+app.listen(port,()=> console.log(`Server run on port http://Localhost:${port}`));
